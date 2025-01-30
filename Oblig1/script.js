b675d6f20b4c65d9beebc23eaecd438569417602
vanDerBC <tobiasbc@hiof.no>
2025-01-30 13:17:08 +0100

    update
    oppdatering av script.js, styles.css og index.html

--- Oblig1/script.js
+++ Oblig1/script.js
@@ -1,5 +1,3 @@
-const nav = document.querySelector("nav")
-
 const ul = document. createElement("ul")
 ul.innerHTML = resources.map(
     (resource, index) => 
@@ -23,8 +21,26 @@ const mainContent = document.getElementById("content")
 function updateMainContent(category, selectedButton) {
 
     buttons.forEach(button => {
-        button.classList.remove("selected")
+        button.classList.toggle("selected", button === selectedButton)
     })
 
-    selectedButton.classList.add("selected")
+    const filteredResources = resources.filter(resource => resource.category === category)
+
+    mainContent.innerHTML = filteredResources.map(resource =>
+        `<article>
+            <h1>${resource.category}</h1>
+            <p>${resource.text}</p>
+            <ul>
+                ${resource.sources.map(source =>
+                    `<li><a href="${source.url}">${source.title}</a></li>`
+                ).join("")}
+            </ul>
+        </article>`
+    )   
+}
+
+const startPage = resources[0].category;
+const filteredPage = Array.from(buttons).filter(button => button.textContent === startPage);
+if (filteredPage.length > 0) {
+    updateMainContent(startPage, filteredPage[0]);
 }
